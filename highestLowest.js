2e364c927ec757558de90e709a0e1cbb8163ddab
nabilk11 <khan.nabiln11.com>
2022-09-15 01:24:52 -0400

    9.15 session contd

---
+++ highestLowest.js
@@ -0,0 +1,47 @@
+// https://www.codewars.com/kata/554b4ac871d6813a03000035/train/javascript
+
+// function highAndLow(numbers) {
+//   let output = [];
+//   let highest = numbers.split(" ").sort((a, b) => a - b).pop()
+//   output.push(highest)
+//   output.push(numbers.split(" ").sort((a, b) => a - b)[0])
+//   return output.join('')
+// }
+
+// function highAndLow(numbers) {
+//   const sortedNums = numbers.split(" ").sort((a, b) => a - b);
+//   const highest = sortedNums[sortedNums.length - 1];
+//   const lowest = sortedNums[0];
+//   return `${highest} ${lowest}`;
+// }
+
+/// best (most efficient) solution
+// function highAndLow(numbers) {
+//   const numStrings = numbers.split(' ')
+
+//   let highest = +numStrings[0];
+//   let lowest = +numbers[0];
+
+//   for(let i = 0; i < numStrings.length; i++) {
+//     const currNum = +numStrings[i];
+
+//     if (currNum > highest) {
+//       highest = currNum
+//     }
+
+//     if (currNum < lowest) {
+//       lowest = currNum
+//     }
+//   }
+
+//   return `${highest} ${lowest}`
+// }
+
+const highAndLow = (s) =>
+  s
+    .split(" ")
+    .sort((a, b) => b - a)
+    .filter((_, i, { length: l }) => i === 0 || i === l - 1)
+    .join(" ");
+
+console.log(highAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4")); // "42 -9"
