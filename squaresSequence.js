a6abbe2d7146c1e8c644f11f56527242a09d0b21
nabilk11 <khan.nabiln11.com>
2022-10-29 13:02:19 -0400

    squares sequence best practice solution

--- squaresSequence.js
+++ squaresSequence.js
@@ -1,6 +1,18 @@
 // https://www.codewars.com/kata/5546180ca783b6d2d5000062/train/javascript - 7 kyu
 
-function squares(x, n) {
+
+// best practice
+
+const squares = (x,n) => {
+    let output = []
+    for (let i = 0; i<n; i++){
+        output.push(x)
+        x*=x
+    }
+    return output
+}
+
+function squares2(x, n) {
   if (n <= 0) return [];
   let output = [x];
   for (let i = 0; i < n - 1; i++) {
@@ -11,5 +23,5 @@ function squares(x, n) {
 
 // array.from method - create an array from an array-like object
 
-const squares = (x, n) =>
+const squares3 = (x, n) =>
   Array.from({ length: n }, (el, idx) => (idx === 0 ? x : (x *= x)));
