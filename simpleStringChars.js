dc501a3fb12691e9a748c5920c04caf07d84e68b
nabilk11 <khan.nabiln11.com>
2022-10-30 15:31:22 -0400

    simple string characters

---
+++ simpleStringChars.js
@@ -0,0 +1,74 @@
+// https://www.codewars.com/kata/5a29a0898f27f2d9c9000058/train/javascript - 7 kyu
+
+// return array with count of uppercase, lowercase, nums, and special chars in a given string
+
+// procedural
+const solve = (s) => {
+  let upper = 0;
+  let lower = 0;
+  let nums = 0;
+  let specChars = 0;
+  let output = [];
+  for (let i = 0; i < s.length; i++) {
+    if (/[a-z]/.test(s[i])) {
+      lower++;
+    }
+    if (/[A-Z]/.test(s[i])) {
+      upper++;
+    }
+    if (/[ `!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/.test(s[i])) {
+      specChars++;
+    }
+    if (/[0-9]/.test(s[i])) {
+      nums++;
+    }
+  }
+  output.push(upper);
+  output.push(lower);
+  output.push(nums);
+  output.push(specChars);
+  return output;
+};
+
+// frequency object with for loop
+const solve2 = (s) => {
+  let charobj = { upper: 0, lower: 0, nums: 0, spec: 0 };
+  for (let i = 0; i < s.length; i++) {
+    if (/[A-Z]/.test(s[i])) {
+      charobj["upper"]++;
+    } else if (/[a-z]/.test(s[i])) {
+      charobj["lower"]++;
+    } else if (/[0-9]/.test(s[i])) {
+      charobj["nums"]++;
+    } else {
+      charobj["spec"]++;
+    }
+  }
+  return Object.values(charobj);
+};
+
+// frequency object with reduce method
+
+const solve3 = (s) => {
+  return Object.values(
+    [...s].reduce(
+      (acc, char) => {
+        if (/[A-Z]/.test(char)) {
+          acc.upper++;
+        } else if (/[a-z]/.test(char)) {
+          acc.lower++;
+        } else if (/[0-9]/.test(char)) {
+          acc.nums++;
+        } else {
+          acc.spec++;
+        }
+        return acc;
+      },
+      { upper: 0, lower: 0, nums: 0, spec: 0 }
+    )
+  );
+};
+
+console.log(solve("Codewars@codewars123.com"));
+console.log(solve2("Codewars@codewars123.com"));
+console.log(solve3("Codewars@codewars123.com"));
