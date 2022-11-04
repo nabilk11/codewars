fef98fd194f4053252cd909db576f300fdc8329a
nabilk11 <khan.nabiln11.com>
2022-11-04 15:08:13 -0400

    is the string uppercase

---
+++ isStringUppercase.js
@@ -0,0 +1,15 @@
+// https://www.codewars.com/kata/56cd44e1aa4ac7879200010b/train/javascript - 8kyu
+
+
+// with String object - String.prototype
+// valueOf gives value of string object
+
+String.prototype.isUpperCase = function () {
+  // your code here
+  let str = this.valueOf();
+  let upper = this.toUpperCase();
+
+  return str === upper;
+};
+
+
