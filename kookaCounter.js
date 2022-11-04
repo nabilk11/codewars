893bf84c21f8912da303d9a051064ba8df1e6c2c
nabilk11 <khan.nabiln11.com>
2022-11-04 14:21:54 -0400

    kooka counter - loop through string & array

---
+++ kookaCounter.js
@@ -0,0 +1,38 @@
+// https://www.codewars.com/kata/58e8cad9fd89ea0c6c000258/train/javascript - 7 kyu
+
+// loop through array
+function kookaCounter(laughing) {
+  // let lower = laughing.toLowerCase()
+  let laughArray = laughing.split("a").filter((item) => item != "");
+  let test = "";
+  let count = 0;
+  console.log(laughArray);
+  for (let i = 0; i < laughArray.length; i++) {
+    test = laughArray[i];
+    if (test !== laughArray[i + 1]) {
+      count += 1;
+    }
+  }
+  return count;
+}
+
+// loop through string
+const kookaCounter2 = (l) => {
+  let count = 0;
+  const hChars = l.replaceAll("a", "");
+  for (let i = 0; i < hChars.length; i++) {
+    if (hChars[i] !== hChars[i + 1]) {
+      count += 1;
+    }
+  }
+  return count;
+};
+
+
+
+console.log(kookaCounter("hahahahaha"));
+console.log(kookaCounter("hahahahahaHaHaHa"));
+console.log(kookaCounter2("hahahahaha"));
+console.log(kookaCounter2("hahahahahaHaHaHa"));
+console.log(kookaCounter3("hahahahaha"));
+console.log(kookaCounter3("hahahahahaHaHaHa"));
