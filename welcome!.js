2129c05aa318dd05291a65e22bd46d5061f25843
nabilk11 <khan.nabiln11.com>
2022-11-06 12:38:16 -0500

    welcome message from db object

---
+++ welcome!.js
@@ -0,0 +1,37 @@
+// https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/javascript - 8 kyu
+// return greeting from object DB
+
+const greetings = {
+  english: "Welcome",
+  czech: "Vitejte",
+  danish: "Velkomst",
+  dutch: "Welkom",
+  estonian: "Tere tulemast",
+  finnish: "Tervetuloa",
+  flemish: "Welgekomen",
+  french: "Bienvenue",
+  german: "Willkommen",
+  irish: "Failte",
+  italian: "Benvenuto",
+  latvian: "Gaidits",
+  lithuanian: "Laukiamas",
+  polish: "Witamy",
+  spanish: "Bienvenido",
+  swedish: "Valkommen",
+  welsh: "Croeso",
+};
+
+
+
+function greet(language) {
+  return greetings[language] || "Welcome";
+}
+
+
+const greet2 = language => greetings[language] || greetings['english'] 
+
+
+console.log(greet("english"));
+console.log(greet("dutch"));
+console.log(greet2("english"));
+console.log(greet2("dutch"));
