cd50634828256082ed92944b02aaf93c98c58290
nabilk11 <khan.nabiln11.com>
2022-11-09 00:18:20 -0500

    create phone numbers - 2 additional solutions - reduce method

--- phoneNumbers.js
+++ phoneNumbers.js
@@ -9,10 +9,25 @@ function createPhoneNumber(numbers) {
   return phoneNumber;
 }
 
-
 const createPhoneNumber2 = (nums) => {
-  const areaCode = nums.slice(0, 3).join('');
-  const mid3 = nums.slice(3, 6).join('');
-  const last4 = nums.slice(6).join('');
-  return `(${areaCode}) ${mid3}-${last4}`
+  const areaCode = nums.slice(0, 3).join("");
+  const mid3 = nums.slice(3, 6).join("");
+  const last4 = nums.slice(6).join("");
+  return `(${areaCode}) ${mid3}-${last4}`;
+};
+
+function createPhoneNumber3(numbers) {
+  let phone = [..."(xxx) xxx-xxxx"];
+  let y = 0;
+  for (let i = 0; i < phone.length; i++) {
+    if (phone[i] === "x") {
+      phone[i] = numbers[y];
+      y++;
+    }
+  }
+  return phone.join("");
 }
+
+// using the reduce method
+const createPhoneNumber4 = (numbers) =>
+  numbers.reduce((acc, curr) => acc.replace("x", curr), "(xxx) xxx-xxxx");
