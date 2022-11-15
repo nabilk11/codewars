bf2fa7eb7b72952b7a62a22b26420652596fd5bf
nabilk11 <khan.nabiln11.com>
2022-11-15 13:29:21 -0500

    sum of digital roots

--- sumDigitalRoots.js
+++ sumDigitalRoots.js
@@ -9,5 +9,26 @@ function numTotal(n) {
   return [...`${n}`].map((e) => +e).reduce((a, b) => a + b);
 }
 
+// without map - not needed
+
+function digitalRoot2(n) {
+  // ...
+  if (n < 10) return n;
+  return digitalRoot(numTotal2(n));
+}
+
+const numTotal2 = (n) => {
+  return [...`${n}`].reduce((a, b) => +a + +b);
+};
+
+
+// without helper method
+
+function digitalRoot3(n) {
+  n = [...`${n}`].reduce((a, b) => +a + +b);
+  if (n < 10) return +n;
+  return digitalRoot3(n);
+}
+
 console.log(digitalRoot(942));
 console.log(digitalRoot(11));
